17cf0108877899d09b96333972a25128291f637e
Filip Raul-Ovidiu <fraulovidiu@gmail.com>
2022-07-12 14:15:04 +0300

    Quizz App

---
+++ quizz_app/script.js
@@ -0,0 +1,31 @@
+const quizData = [
+    {
+        question: 'How old is your dog?',
+        a: '10',
+        b: '198',
+        c: '2',
+        d: '0.5',
+        correct: 'c'
+    }, {
+        question: 'What car do you like?',
+        a: 'BMW',
+        b:' Mercedes-Benz',
+        c: 'Volvo',
+        d: 'Audi',
+        correct:'a',
+    }, {
+        question: 'What is 7 x 8?',
+        a: '7',
+        b: '8',
+        c: '56',
+        d: '100',
+        correct:'c',
+    }, {
+        question: 'How many corners are in a circle?',
+        a: '1',
+        b: '2',
+        c: '3',
+        d: '0',
+        correct: 'd',
+    }
+]
